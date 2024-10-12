eb33edb9e0f0b069042f9be4d64128e597055740
ArvinGlenn-02 <2201102850@student.buksu.edu.ph>
2024-10-12 21:07:06 +0800

    Add updated_sample.java file

---
+++ updated_sample.java
@@ -0,0 +1,14 @@
+public class updated_sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, Backend World!");
+        
+        // Additional backend logic
+        int result = add(5, 3);
+        System.out.println("The result of addition is: " + result);
+    }
+
+    // method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
